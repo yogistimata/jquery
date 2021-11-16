3107fc7bce021823fd1fb0eed9720e79d5267168
yogistimata <90698851+yogistimata@users.noreply.github.com>
2021-11-16 15:22:27 +0700

    belajar membuat kontak

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready (function(){
+    $('.bd').css ('background-color' , 'blue');
+
+    $('.contact').css ('background-color', 'aqua');
+    $('.contact').css ('width', '85%');
+    $('.contact').css ('max-width', '600px');
+    $('.contact').css ('position', 'absolute');
+    $('.contact').css ('left', '50%');
+    $('.contact').css ('transform', 'translate(-50%)');
+    $('.contact').css ('padding', '30px 40px');
+    $('.contact').css ('box-sizing', 'border-box');
+    $('.contact').css ('border-radius', '8px');
+    $('.contact').css ('text-align', 'center');
+    $('.contact').css ('box-shadow', '20px');
+    $('.contact').css ('font-family', '"Montserrat",sans-serif');
+   
+   
+
+    $('h1').css ( 'color' , 'red',
+        'font-weight', '200');
+
+    $('.txtb').css ('border','1px solid gray');
+    $('.txtb').css ('margin', '8px 0');
+    $('.txtb').css ('padding', ' 14px');
+    $('.txtb').css ('border-radius', '5px');
+    
+    $('.txtb label').css ('display', 'block');
+    $('.txtb label').css ('width','0px');
+    $('.txtb label').css ('text-align', 'left');
+    $('.txtb label').css ('color', '#333');
+    $('.txtb label').css ('text-transform', 'uppercase');
+    $('.txtb label').css ('font-size', '14px');
+    
+    $('.txtb input,.txtb textarea').css ('width', '100%');
+    $('.txtb input,.txtb textarea').css ('border', 'none');
+    $('.txtb input,.txtb textarea').css ('outline', 'none');
+    $('.txtb input,.txtb textarea').css ('font-size', '18px');
+    $('.txtb input,.txtb textarea').css ('margin-top', '50px');
+
+
+    
+    $('.btn').css ('display', 'inline-block');
+    $('.btn').css ('padding', '14px 0');
+    $('.btn').css ('text-transform', 'uppercase');
+    $('.btn').css ('cursor', 'pointer');
+    $('.btn').css ('margin-top', '8px');
+    $('.btn').css ('width', '100%');
+    $('.btn').css ( 'background', 'gold');
+    $('.btn').css ('color', 'red');
+        
+    
+})
